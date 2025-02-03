d417d5ad1f6186c54a649dba406da160d78767af
ongudidan <ongudidan@gmail.com>
2025-02-03 23:49:41 +0300

    modified:   modules/dashboard/views/categories/index.php

--- modules/dashboard/views/expense-categories/index.php
+++ modules/dashboard/views/expense-categories/index.php
@@ -15,7 +15,7 @@ use yii\widgets\Pjax;
 
 $this->title = 'Expense Categories';
 $this->params['breadcrumbs'][] = $this->title;
-$this->params['modalSize'] = \yii\bootstrap5\Modal::SIZE_SMALL;
+$this->params['modalSize'] = \yii\bootstrap5\Modal::SIZE_DEFAULT;
 
 ?>
 
